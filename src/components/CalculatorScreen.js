51974e01474053a6709233606c8d88afb574a9b0
solmoyano <solmoyano1012@gmail.com>
2022-06-30 21:57:39 -0300

    version 1.0

---
+++ src/components/CalculatorScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CalculatorScreen (){
+
+    return(
+        <div>
+            Soy la pnatalla
+        </div>
+    )
+}
+export default CalculatorScreen;
---
+++ src/components/CalculatorStructure.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CalculatorScreen from './CalculatorScreen';
+
+function CalculatorStructure (){
+
+    return(
+        <div>
+            <CalculatorScreen></CalculatorScreen>
+        </div>
+    )
+}
+export default CalculatorStructure;
